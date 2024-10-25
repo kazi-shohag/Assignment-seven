9ace15a19997e0a8885e415f96938f65e995a4a2
kazi-shohag <kazishohag1166@gmail.com>
2024-10-25 10:59:27 +0600

    first commite

---
+++ public/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+
+const Header=()=>{
+    return (
+        <div>
+            <h1>Header section</h1>
+        </div>
+    )
+}
+
+export default Header;
---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import Header from '../public/components/Header/Header'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      
+      <h1 className='font-extralight bg-slate-950'>BpL Players Sellect Site.</h1>
+      <Header></Header>
+      
+    </>
+  )
+}
+
+export default App
